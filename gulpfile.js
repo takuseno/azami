e7220b25b2507c33a6b0c997a19ab99254cc8abe
takuseno <takuma.seno@gmail.com>
2016-04-08 21:41:06 +0900

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp')
+var babel = require('gulp-babel');
+
+gulp.task('compile', () => {
+    return gulp.src('src/**/*.{js,jsx}')
+        .pipe(babel())
+        .pipe(gulp.dest('dist'));
+})
+
---
+++ src/constants/Constants.js
@@ -0,0 +1,6 @@
+import * as KeyMirror from 'keymirror';
+
+export default  KeyMirror({
+    CHANGE_REPOSITORY: null,
+    CLICK_PULL_REQUEST: null
+});
