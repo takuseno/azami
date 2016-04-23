6194e0c876bd4234d080bc459ea878b4b3bf83e5
takuseno <takuma.seno@gmail.com>
2016-04-23 18:29:18 +0900

    Enable pull request api to get all pull requests

--- src/utils/GitHubApiUtils.js
+++ src/utils/GitHubApiUtils.js
@@ -18,7 +18,6 @@ export default class GitHubApiUtils {
               .map(GitHubDataUtils.convertRawRepository)
               .toArray()
             repositories = repositories.concat(converted)
-            console.log(result.nextUrl)
             const nextUrl = result.nextUrl
             if (nextUrl === undefined) {
               AppActions.loadRepositoriesCompleted(repositories)
@@ -39,13 +38,22 @@ export default class GitHubApiUtils {
       repository: repository.name
     }
     try {
-      PullRequestApi.getAll(parameters)
-        .then((pullRequests) => {
-          const converted = Immutable.Seq(pullRequests)
-            .map(GitHubDataUtils.convertRawPullRequest)
-            .toArray()
-          AppActions.loadPullRequestsCompleted(converted)
-        })
+      let pullRequests = []
+      Promise.resolve(undefined).then(function loop (url) {
+        PullRequestApi.getAll(parameters, url)
+          .then((result) => {
+            const converted = Immutable.Seq(result.pullRequests)
+              .map(GitHubDataUtils.convertRawPullRequest)
+              .toArray()
+            pullRequests = pullRequests.concat(converted)
+            const nextUrl = result.nextUrl
+            if (nextUrl === undefined) {
+              AppActions.loadPullRequestsCompleted(pullRequests)
+            } else {
+              loop(nextUrl)
+            }
+          })
+      })
     } catch (e) {
       AppActions.error(e)
     }
